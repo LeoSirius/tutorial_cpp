b20f00d12c03ff907642157445dac4116c338aa5
LeoSirius <is.li.xiaoyu@qq.com>
2020-11-14 23:17:05 +0800

    update

--- 侯捷视频教程集合/1_c++面向对象高级开发/complex.h
+++ 侯捷视频教程集合/1_c++面向对象高级开发/complex.h
@@ -7,12 +7,14 @@
 class complex
 {
 public:
-    complex (double r = 0, T i = 0)
+    complex (double r = 0, double i = 0)
       : re(r), im(i)
     {}
-    complex& operator += (const complex&);
-    double real () const { return re; }
+    complex& operator += (const complex&);   // 无{}的是声明
+    double real () const { return re; }      // 有{}的是定义，这里的函数就是inline的，inline函数类似宏
     double imag () const { return im; }
+
+    // 
 
 private:
     double re, im;
